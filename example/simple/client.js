f736c7763efee79f412909b66a185b37fc2b6528
Ivo Georgiev <ivo@linvo.org>
2014-02-22 19:33:56 +0200

    test date in the simple example

--- example/simple/client.js
+++ example/simple/client.js
@@ -1,8 +1,8 @@
 var dnode = require('../../');
 
 dnode.connect(7070, function (remote, conn) {
-    remote.zing(33, function (n) {
-        console.log('n=' + n);
+    remote.zing(33, function (n, d) {
+        console.log('n=' + n+" at "+d.getTime());
         conn.end();
     });
 });
--- example/simple/server.js
+++ example/simple/server.js
@@ -1,6 +1,6 @@
 var dnode = require('../../');
 
 var server = dnode(function (remote, conn) {
-    this.zing = function (n, cb) { cb(n * 100) };
+    this.zing = function (n, cb) { cb(n * 100, new Date()) };
 });
 server.listen(7070);
